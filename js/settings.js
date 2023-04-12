08c26ddaf0975dd50f515af1e738e8139a165171
dablju <wiktordablju@gmail.com>
2023-04-12 23:24:06 +0200

    Update Specjalizacja

--- js/settings.js
+++ js/settings.js
@@ -1,27 +1,27 @@
 // Funkcje na przekierowywanie do odpwowiednich stron na ustawienia
 
-const loginBtn = document.querySelector("#login-btn")
-const registerBtn = document.querySelector("#register-btn")
-const deleteAccBtn = document.querySelector("#delete-acc-btn")
-const changeLanguageBtn = document.querySelector("#change-language-btn")
+const loginBtn = document.querySelector("#login-btn");
+const registerBtn = document.querySelector("#register-btn");
+const deleteAccBtn = document.querySelector("#delete-acc-btn");
+const changeLanguageBtn = document.querySelector("#change-language-btn");
 
 function login() {
-	window.location.href = "./../index.html"
+	window.location.href = "./../html/login.html";
 }
 
 function register() {
-	window.location.href = "./../index.html"
+	window.location.href = "./../html/register.html";
 }
 
 function deleteAcc() {
-	window.location.href = "./../index.html"
+	window.location.href = "./../html/delete_account.html";
 }
 
 function changeLanguage() {
-	window.location.href = "./../index.html"
+	window.location.href = "./../index.html";
 }
 
-loginBtn.addEventListener("click", login)
-registerBtn.addEventListener("click", register)
-deleteAccBtn.addEventListener("click", deleteAcc)
-changeLanguageBtn.addEventListener("click", changeLanguage)
+loginBtn.addEventListener("click", login);
+registerBtn.addEventListener("click", register);
+deleteAccBtn.addEventListener("click", deleteAcc);
+changeLanguageBtn.addEventListener("click", changeLanguage);
